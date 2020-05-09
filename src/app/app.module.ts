da63050ca6fb5a00f775ab2440c1d8278bcb5e29
tkBinh <binh13it@gmail.com>
2020-05-10 02:28:48 +0700

    Add font-AS, mdb-bootstrap, config ussing scss, bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {ThemeModule} from './@theme/theme.module';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {ThemeModule} from './@theme/theme.module';
         BrowserModule,
         AppRoutingModule,
         ThemeModule,
+        MDBBootstrapModule
     ],
   providers: [],
   bootstrap: [AppComponent]
